6c47f6873369f5d71995e3083d1162de78b1fbe6
CD丶wOw <48509493+caidix@users.noreply.github.com>
2023-03-27 11:24:12 +0800

    feat: 修改nav

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -45,8 +45,6 @@ module.exports = [
     link: "/tools/",
     items: [
       { text: "技术文档", link: "/pages/9a7ee40fc232253e/" },
-      { text: "GitHub技巧", link: "/pages/4c778760be26d8b3/" },
-      { text: "常用方法", link: "/pages/6aa903/" },
     ],
   },
   {
